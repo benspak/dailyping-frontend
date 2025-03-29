fe756171d8e62585b3f09e231652cffd8a68ac6c
Ben S <89033552+benspak@users.noreply.github.com>
2025-03-29 14:20:40 -0600

    [Updates] Styling  changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,24 +12,16 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
       <div className="container">
-        <Link className="navbar-brand" to="/">DailyPing</Link>
-        <div className="collapse navbar-collapse">
+        <a className="navbar-brand" href="/">DailyPing</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
-            {token && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/dashboard">Dashboard</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/respond">Respond</Link>
-                </li>
-                <li className="nav-item">
-                  <button onClick={handleLogout} className="btn btn-outline-secondary btn-sm ms-2">Logout</button>
-                </li>
-              </>
-            )}
+            <li className="nav-item"><a className="nav-link" href="/dashboard">Dashboard</a></li>
+            <li className="nav-item"><button className="btn btn-outline-secondary" onClick={logout}>Logout</button></li>
           </ul>
         </div>
       </div>
