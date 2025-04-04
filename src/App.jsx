0c87cd9c8391350930b8b8b1a80299de66479d9e
Ben S <89033552+benspak@users.noreply.github.com>
2025-04-03 23:27:04 -0600

    [Up] Settings

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
           } />
           <Route path="/settings" element={
             <PrivateRoute>
-              <ProSettings />
+              <Settings />
             </PrivateRoute>
           } />
           <Route path="/calendar" element={<PrivateRoute>
