44a24bd2e7a2578a1e5660020b08ac8bbe9c004f
Ben S <89033552+benspak@users.noreply.github.com>
2025-04-10 12:46:23 -0600

    [Fix] import user auth correctly

--- src/pages/ProjectForm.jsx
+++ src/pages/ProjectForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import useAuth from "../context/AuthContext"
+import { useAuth } from "../context/AuthContext"
 
 export default function ProjectForm() {
   const { user } = useAuth;
